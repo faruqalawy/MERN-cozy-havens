dc757d8168efba3eabeac6eb1abeba79fddaceca
faruqalawy <faruqhudaya@gmail.com>
2024-08-30 18:14:41 +0700

    change api from local to vercel

--- frontend/src/pages/LandingPages.js
+++ frontend/src/pages/LandingPages.js
@@ -32,8 +32,8 @@ class LandingPage extends Component {
   async loadData() {
     try {
       const [mostPickedResponse, categoriesResponse] = await Promise.all([
-        axios.get("https://mern-cozy-havens.vercel.app/mostPicked"),
-        axios.get("https://mern-cozy-havens.vercel.app/categories"),
+        axios.get("https://mern-cozy-havens-backend.vercel.app/mostPicked"),
+        axios.get("https://mern-cozy-havens-backend.vercel.app/categories"),
       ]);
 
       console.log("MostPicked Data:", mostPickedResponse.data);
